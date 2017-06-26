4390d861a039f5fdbfa3f1c2e7c515a6c784ffa3
Simon Templer <s.bury@phenoma.ly>
2017-06-26 21:22:22 +0200

    part 2 - SingleGame matching

--- src/pages/games/singleGame/singleGame.ts
+++ src/pages/games/singleGame/singleGame.ts
@@ -4,6 +4,8 @@ import { DataService } from '../../../services';
 import { RSVPGame } from '../../../models';
 
 import moment from 'moment';
+import { ShareService } from '../../../services/ShareService';
+import { User } from '../../../models/user';
 
 @Component({
     selector: 'single-game',
@@ -14,23 +16,36 @@ export class SingleGame implements OnInit {
 
     private id: number;
     game: RSVPGame;
-    canSetNotification: boolean = true;
+    user: User;
+    // canSetNotification: boolean = true;
 
-    rsvpStatus: string | number = null;
+    rsvpStatus: string | number;
 
     constructor(
         private dataService: DataService,
-        private navParams: NavParams
+        private navParams: NavParams,
+        private share: ShareService
     ) {
         this.id = this.navParams.get('id');
     }
 
     ngOnInit() {
+        this.user = this.share.getUser();
+
+        console.log('SingleGame ngOnInit user', this.user);
+
         this.dataService.games.getSingle(this.id)
             .then(game => {
-                console.log(game);
+                console.log('SingleGame ngOnInit game', game);
                 this.game = game;
-                this.rsvpStatus = game.players[0].rsvp;
+                game.players.find(player => {
+                    console.log('SingleGame ngOnInit player, user', player, this.user);
+                    if (player.id === this.user.id) {
+                        console.log('SingleGame ngOnInit player rsvp', player.rsvp);
+                        this.rsvpStatus = player.rsvp;
+                    }
+                });
+                console.log('SingleGame ngOnInit rsvpStatus', this.rsvpStatus);
                 let gameTime = new Date(this.game.datetime);
                 this.game.datetime = moment(gameTime).format('LLLL');
             });
@@ -42,8 +57,24 @@ export class SingleGame implements OnInit {
             return;
         }
         // todo optimize initial value sending
-        this.dataService.games.updateRSVP(this.id, this.game.players[0].rsvp_id, Number(e))
-            .catch(err => console.log('SingleGame statusChanged err', err))
-            .then(() => this.game.players[0].rsvp = Number(e));
+
+        console.log('SingleGame statusChanged', e);
+
+        let index;
+        let player = this.game.players.find((player, i) => {
+            if (player.id === this.user.id) {
+                console.log('SingleGame statusChanged found', player, i);
+                index = i;
+                return player;
+            }
+        });
+
+
+        if (player) {
+            this.dataService.games.updateRSVP(this.id, player.rsvp_id, Number(e))
+                .catch(err => console.log('SingleGame statusChanged err', err))
+                .then(() => this.game.players[index].rsvp = Number(e));
+        }
+
     }
 }
